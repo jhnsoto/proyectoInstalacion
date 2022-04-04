e8b3b65c976daff1dad85f3aad12e84ca67285ba
jhnsoto <88836958+jhnsoto@users.noreply.github.com>
2022-04-04 18:06:43 -0500

    Update a sketch

--- sketch.js
+++ sketch.js
@@ -9,9 +9,9 @@ function draw() {
   for(var y = 0; y<height;y++){
     for(var x = 0; x<width;x++){
       var index = (x+ y*width)*4;
-      pixels[index] = 25;
+      pixels[index] = 100;
       pixels[index+1] = 200 +random(55);
-      pixels[index+2] = 50;
+      pixels[index+2] = 100;
       pixels[index+3] = 255;
     }
   }
